c21b05632a6f48e237dc336351e0432c92d5b276
Muhammad Abdullahi <muhammadkaasim3@gmail.com>
2024-08-26 15:04:17 +0300

    google analytics is added

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,41 @@
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import './globals.css'
-import { Inter } from 'next/font/google';
-import Header from '@/components/Header';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Script from "next/script";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <title>Flashkards</title>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-GCLLH8MDWL"
+          ></Script>
+          <Script id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-GCLLH8MDWL');`}
+          </Script>
+        </head>
         <body className={inter.className}>
           <Header />
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
